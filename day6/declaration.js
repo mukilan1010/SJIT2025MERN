74f5713e2facf52e0719a86cdd3f84ad90865d8e
mukilan1010 <146624884+mukilan1010@users.noreply.github.com>
2025-02-17 08:15:17 +0530

    day6 an

--- day6/declaration.js
+++ day6/declaration.js
@@ -1,6 +1,7 @@
 // console.log("hello")
 
 // declarations
+//primary datatypes
 // a=10
 // b=15
 // c=true
@@ -9,21 +10,22 @@
 // f='h'
 // console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(h))
 
-// var a=10;
-// let b="hi"
-// const c=true
+// var a=10;  //global scope
+// let b="hi" //local scope
+// const c=true //cannot be modified
 // console.log(c)
 // c=false
 
 
-// //  --secondary datatypes
+//  --secondary datatypes
 
-// // 1)array
+// 1)array
 // arr=[1,23.3,"hi"]
 // console.log(arr[1])
+// console.log(typeof(arr)) // all the secondary datatypes are recognized as object type
 
-// // 2)object
-// // type 1)
+// 2)object
+// type 1)
 // obj1={
 //     firstname:"st.Joseph's",
 //     lastname:"institute",
@@ -32,29 +34,135 @@
 // }
 // console.log(obj1)
 
-// // type 2)
-
+// type 2)
 // obj2={}
 // obj2["firstname"]="hello"
 // obj2["lastname"]="world"
-// console.log(obj2)
+// obj2["training"]=["GIT","HTML","CSS","JS","mern"]
+// console.log(obj2) //takes the space of the system while printing
 
 // console.log(obj2.firstname)
 // console.log(obj2["firstname"]) //quotes compulsory(single or double)
 
-// type 3
+// type 3)
+// obj3=new Object()
+// console.log(obj3)
+// obj3.training="mern"
+// obj3.dept=["cse","it","ads"]
+// console.log(obj3) 
 
-// object3=new object()
-// object3.training=
-// console.log(object3);
+// 3)set
+// set=new Set()
+// console.log(set)
+// set=new Set("hello")
+// console.log(set)
+// set=new Set(["hello"])
+// console.log(set)
+// set.add("hi") //added based on the prev added type
+// console.log(set)
+// set.add("hi")
+// console.log(set) //no duplicates added
 
-// set
-set=new Set()
-console.log(set);
-set.add("Hello");
-console.log(set);
-set.add("mukilan")
-console.log(set);
-console.log(typeof(set));
+//logical operators
+// console.log(5 && 10)
+// console.log(10 && 5)
+// console.log(true && 10)
+// console.log(false && 10)
+// console.log(false || 10)
+// console.log(true || 5)
+// console.log(5 || true)
+// console.log(5 || 10)
+// console.log(10 || 5)
+// console.log(false && true)
+// console.log(true && false)
+// console.log(false || true)
+// console.log(true || false)
 
+//Relational Operators
+// a='5'
+// b=5
+// console.log("This is == operator",a==b); // datatype is not consider
+// console.log("this is === operator",a===b); //datatype is consider
 
+// //bitwise 
+// console.log(70&35);
+// console.log(70^35);
+// c=1
+// console.log(~c);
+
+//conditional statement
+//1)simple if
+//2)if....else..
+//3)nested if
+//4)else if ladder
+//5)switch
+
+//looping statements
+//1)while
+//2)do...while   (; not mandatory after while)
+//3)for loop (use let n)
+//4)for in
+//5)for of
+//6)for Each
+
+//4)for in
+// arr=[10,20,30,"abc",true,"hello",[10,20,30]]
+// for (a in arr){  //(var a is not necessary)
+//     console.log(a)
+// } //prints the keys
+
+// obj1={
+//         firstname:"st.Joseph's",
+//         lastname:"institute",
+//         depts:["CSE","ADS","IT","ECE"],
+//         training:"mern"
+//     }
+// for(i in obj1){
+//     console.log(i) //prints the keys
+// }
+
+// for(i in obj1){
+// console.log(obj1[i])
+// }
+
+// //5)for of
+// arr=[10,20,30,"abc",true,"hello",[10,20,30]]
+// for (a of arr){  //(var a is not necessary)
+//     console.log(a)
+// } //prints the value at the key
+
+// for(i of obj1){
+//     console.log(i)  //non iterable
+// }
+
+// for(i of Object.values(obj1)){
+//     console.log(i)  //prints the values resp
+// }
+// console.log("\n")
+// for({k,v} of Object.entries(obj1)){
+//     console.log(k,v)  //prints the keys and values resp
+// }
+// console.log("\n")
+// for([k,v] of Object.entries(obj1)){
+//     console.log(k,v)  //prints all keys and values as undefined why??(task 1)
+// }
+
+//for each
+// arr.forEach((ele)=>{
+//     console.log(ele)
+// })
+
+// Object.keys(obj1).forEach((ele)=>{
+//     console.log(ele)
+// })
+
+// Object.entries(obj1).forEach((key,val)=>{
+//     console.log(key,val) //why 0,1,2,3 is printed? task4
+// })
+
+//functions
+// 1)without parameter without argument
+// function testfunc(){
+//     console.log("this is a test func")
+// }
+// testfunc()
