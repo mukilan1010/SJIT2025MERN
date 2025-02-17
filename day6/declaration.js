b8a410282ad9958779627450b14762359c5c4df3
mukilan1010 <146624884+mukilan1010@users.noreply.github.com>
2025-02-17 08:13:56 +0530

    day 6 fn

---
+++ day6/declaration.js
@@ -0,0 +1,60 @@
+// console.log("hello")
+
+// declarations
+// a=10
+// b=15
+// c=true
+// d=10.345
+// e="hi"
+// f='h'
+// console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(h))
+
+// var a=10;
+// let b="hi"
+// const c=true
+// console.log(c)
+// c=false
+
+
+// //  --secondary datatypes
+
+// // 1)array
+// arr=[1,23.3,"hi"]
+// console.log(arr[1])
+
+// // 2)object
+// // type 1)
+// obj1={
+//     firstname:"st.Joseph's",
+//     lastname:"institute",
+//     depts:["CSE","ADS","IT","ECE"],
+//     training:"mern"
+// }
+// console.log(obj1)
+
+// // type 2)
+
+// obj2={}
+// obj2["firstname"]="hello"
+// obj2["lastname"]="world"
+// console.log(obj2)
+
+// console.log(obj2.firstname)
+// console.log(obj2["firstname"]) //quotes compulsory(single or double)
+
+// type 3
+
+// object3=new object()
+// object3.training=
+// console.log(object3);
+
+// set
+set=new Set()
+console.log(set);
+set.add("Hello");
+console.log(set);
+set.add("mukilan")
+console.log(set);
+console.log(typeof(set));
+
+
